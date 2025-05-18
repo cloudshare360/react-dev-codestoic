d734fa652f57b71f6bdc5796ad187c5f00458de5
Cloud Share <cloud.share360@gmail.com>
2025-05-18 01:24:25 +0000

    updated

---
+++ ReactJSTutorial-39-TodoAppPart12StylingTodoItems/todoapp/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Todo from "./components/Todo";
+import Header from "./components/Header"
+function App() {
+
+
+  return (
+    <div className="App">
+      <Header />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <Todo />
+    </div>
+  )
+}
+
+export default App
---
+++ ReactJSTutorial-39-TodoAppPart12StylingTodoItems/todoapp/src/components/Todo.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Form from './Form'
+import  TodoList from "./TodoList";
+
+
+export default function Todo() {
+
+  const [todos, setTodos] = useState(["play", "lunch", "homework"]);
+
+
+  return (
+    <div>
+      <p> ReactJSTutorial-39-TodoAppPart12StylingTodoItems</p>
+      <Form todos={todos} setTodos={setTodos}/>
+      <TodoList todos={todos}/>
+    </div>
+  );
+}
