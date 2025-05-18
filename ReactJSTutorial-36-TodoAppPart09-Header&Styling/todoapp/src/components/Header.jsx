a7ab7f292622731dcff630559ef5e50c3c5f7080
cloudshare360 <79122392+cloudshare360@users.noreply.github.com>
2025-05-18 01:26:45 +0000

    updated

--- ReactJSTutorial-36-TodoAppPart09-Header&Styling/todoapp/src/components/Header.jsx
+++ ReactJSTutorial-36-TodoAppPart09-Header&Styling/todoapp/src/components/Header.jsx
@@ -1,6 +1,5 @@
-import style from "./header.module.css"
+import style from "./header.module.css";
 
-
-export default function Header(){
-    return <div className={style.Header}><h1>Todo List App</h1></div>
+export default function Header() {
+  return <div className={style.header}>Todo List App</div>;
 }
